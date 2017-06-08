e8c0557cb1c12904d0c4e1eed506d827ee18e572
zeroryuu <liujk1225@gmail.com>
2017-06-08 10:41:03 +0900

    delete WebSecurityConfig.java

--- src/main/java/com/security/config/WebSecurityConfig.java
+++ src/main/java/com/security/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		System.out.println("configure");
 		http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll()
 				.antMatchers("/users/**").hasRole("USER")
 				.antMatchers("/admins/**").hasRole("ADMIN")
@@ -43,7 +41,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	@Override
 	protected UserDetailsService userDetailsService() {
-		System.out.println("cEEEEEEEe");
 		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 		manager.createUser(User.withUsername("user").password("user").roles("USER").build());
 		manager.createUser(User.withUsername("admin").password("admin").roles("ADMIN").build());
@@ -53,7 +50,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		System.out.println("adfsdf");
 	    auth.userDetailsService(userDetailsService());
 	}
 	
